d8a4cad7bad3e419fd8707a818fb1d5c0d57f93b
Gabriel Wolf <gabeericwolf@gmail.com>
2024-03-20 10:40:53 -0400

    show the ones that fail

--- tests.rkt
+++ tests.rkt
@@ -12,25 +12,25 @@
 (check-equal? (interpret "test3.bad") 10)
 (check-equal? (interpret "test4.bad") 16)
 (check-equal? (interpret "test5.bad") 220)
-(check-equal? (interpret "test6.bad") 5)
+(check-equal? (interpret "test6.bad") 5) ; fail
 (check-equal? (interpret "test7.bad") 6)
 (check-equal? (interpret "test8.bad") 10)
 (check-equal? (interpret "test9.bad") 5)
 (check-equal? (interpret "test10.bad") -39)
 (check-exn
-   exn:fail? (lambda () (interpret "test11.bad")))
+   exn:fail? (lambda () (interpret "test11.bad"))) ; fail
 (check-exn
    exn:fail? (lambda () (interpret "test12.bad")))
 (check-exn
    exn:fail? (lambda () (interpret "test13.bad")))
 (check-exn
-   exn:fail? (lambda () (interpret "test14.bad")))
+   exn:fail? (lambda () (interpret "test14.bad"))) ; fail
 (check-equal? (interpret "test15.bad") 'true)
 (check-equal? (interpret "test16.bad") 100)
-(check-equal? (interpret "test17.bad") 'false)
-(check-equal? (interpret "test18.bad") 'true)
-(check-equal? (interpret "test19.bad") 128)
-(check-equal? (interpret "test20.bad") 12)
+(check-equal? (interpret "test17.bad") 'false) ; fail
+(check-equal? (interpret "test18.bad") 'true) ; fail
+(check-equal? (interpret "test19.bad") 128) ; fail
+(check-equal? (interpret "test20.bad") 12) ; fail
 
 
 ; update-binding tests
