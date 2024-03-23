d148f7a81ef9fe4f4c4065185bf7aac0b1acbada
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2024-03-22 22:57:48 -0400

    Cleaned Up Code (#63)
    * Cleaned up bottom of state functions through M_statements
    
    * Finished cleaning up state functions
    
    * Formatted valueFunctions
    
    * Finished Cleaning up the code
    
    * this was renamed
    
    * last clean
    
    ---------
    
    Co-authored-by: Ethan <1ethanhansen@protonmail.com>

--- utils.rkt
+++ utils.rkt
@@ -49,42 +49,27 @@
   (lambda (layers)
     (combine empty-layer layers)))
 
-; Removes the 1st layer from the front of the list of layers
+; [DEPRECATED] Removes the 1st layer from the front of the list of layers
 (define remove-layer-old
   (lambda (layers)
     (if (null? layers)
         null
         (cdr layers))))
 
-
+; Removes the 1st layer from the front of the list of layers
 (define remove-layer
   (lambda (layers)
     (cond
       ((null? layers) null)
       (else
        (cdr layers)))))
-    
 
 ; Add Binding to the state
 (define add-binding
   (lambda (name value state)
-  (cond
-      ((null? state) (list (cons (list name) (list (list value)))))
-      (else
-       (cons (cons (cons name (caar state)) (list (cons value (cadar state)))) (cdr state)) 
-       ))))
-       
-
-(define add-to-layer
-  (lambda (name value layer)
-    (list
-     (if (null? (car layer))
-         (list name)
-         (cons name (car layer)))
-     (if (null? (cadr layer))
-         (list value)
-         (cons value (cadr layer))))))
-
+  (if (null? state)
+      (list (cons (list name) (list (list value))))
+      (cons (cons (cons name (caar state)) (list (cons value (cadar state)))) (cdr state)))))
 
 
 ; update binding
@@ -100,7 +85,6 @@
              (cons result (cdr state))))) ; return the value if found
     (else (error "state is bad")))))
 
-
 (define update-binding-helper
   (lambda (vars keys value newvalue return)
     (cond
@@ -114,7 +98,6 @@
                               (lambda (r-vars r-keys status)
                                 (return (cons (car vars) r-vars) (cons (car keys) r-keys) status)))))))
 
-
 ; verify the state has multiple layers
 (define check_break
   (lambda (state)
@@ -122,7 +105,6 @@
       ((null? state) 'error) ; state has no layers
       ((null? (cdr state)) 'error) ; state is only one layer
       (else state)))) ;; state has multiple layers
-
 
 ; add 'begin to try
 (define add_begin_try
