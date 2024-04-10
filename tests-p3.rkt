d546881c65c1b9e9a80b3240ca6c04a2b59fa2d8
Ethan Hansen <1ethanhansen@protonmail.com>
2024-04-10 13:43:07 -0400

    7 and 18 work (#122) (closes #105 closes #114)

--- tests-p3.rkt
+++ tests-p3.rkt
@@ -10,7 +10,7 @@
 (check-equal? (interpret "tests/p3_t4.bad") 55)
 ;(check-equal? (interpret "tests/p3_t5.bad") 1) ; Fail
 ;(check-equal? (interpret "tests/p3_t6.bad") 115) ; Fail
-;(check-equal? (interpret "tests/p3_t7.bad") true) ; Fail
+(check-equal? (interpret "tests/p3_t7.bad") 'true) ; Fail
 ;(check-equal? (interpret "tests/p3_t8.bad") 20) ; Fail
 (check-equal? (interpret "tests/p3_t9.bad") 24)
 ;(check-equal? (interpret "tests/p3_t10.bad") 2) ; Fail
@@ -21,7 +21,7 @@
 ;(check-equal? (interpret "tests/p3_t15.bad") 87) ; Fail
 ;(check-equal? (interpret "tests/p3_t16.bad") 64) ; Fail
 (check-equal? (interpret "tests/p3_t17.bad") error "b out of scope")
-;(check-equal? (interpret "tests/p3_t18.bad") 125) ; Fail
+(check-equal? (interpret "tests/p3_t18.bad") 125)
 ;(check-equal? (interpret "tests/p3_t19.bad") 100) ; Fail
 ;(check-equal? (interpret "tests/p3_t20.bad") 2000400) ; Fail
 ;(check-equal? (interpret "tests/p3_t21.bad") 2) ; Fail
